97f5f9b10ac1697ff51d5db590b91fff637ee605
Francois Boivin <boivin.92@gmail.com>
2020-08-23 23:38:20 -0400

    inputs now saved between sessions

--- UI/Controls/CsvMethod.gd
+++ UI/Controls/CsvMethod.gd
@@ -1,22 +1,25 @@
 extends TabContainer
 
+onready var file_path_label = $FILE_TAB_LABEL/MarginContainer/VBoxContainer/HBoxContainer/FileNameLabel
 onready var tree : Tree = $FILE_TAB_LABEL/MarginContainer/VBoxContainer/Tree
+onready var raw_text_input = $TEXT_TAB_LABEL/Textbox
 var separator = ","
 
-var file_content_as_array
+var file_content_as_array = []
+const last_inputs = "user://last_inputs.json"
 
 signal browse
 
 func _ready():
-	pass
+	load_inputs()
 
 func get_csv_in_use():
 	if current_tab == 0:
 		return file_content_as_array
 	if current_tab == 1:
 		var lines = []
-		for line in $TEXT_TAB_LABEL/Textbox.get_line_count():
-			lines.append($TEXT_TAB_LABEL/Textbox.get_line(line).split(separator, true))
+		for line in raw_text_input.get_line_count():
+			lines.append(raw_text_input.get_line(line).split(separator, true))
 		return lines
 
 func expose_csv(csv : Array):
@@ -32,7 +35,7 @@ func expose_csv(csv : Array):
 
 func _on_FileDialog_file_selected(path):
 	expose_csv(get_lines_from_file(path))
-	$FILE_TAB_LABEL/MarginContainer/VBoxContainer/HBoxContainer/FileNameLabel.text = path
+	file_path_label.text = path
 
 func get_lines_from_file(path):
 	var file = File.new()
@@ -48,6 +51,36 @@ func get_lines_from_file(path):
 	file_content_as_array = values
 	return values
 
-
 func _on_BrowseButton_pressed():
 	emit_signal("browse")
+
+func _on_CsvMethod_tree_exiting():
+	save_inputs()
+
+func save_inputs():
+	var inputs = {
+		"file" : {
+			"selected_file" : file_path_label.text,
+			"data" : file_content_as_array
+			},
+		"raw_text" : raw_text_input.text
+	}
+	var save = File.new()
+	save.open(last_inputs, File.WRITE)
+	save.store_line(to_json(inputs))
+	save.close()
+
+func load_inputs():
+	var file = File.new()
+	if not file.file_exists(last_inputs):
+		save_inputs()
+	file.open(last_inputs, File.READ)
+	var content = file.get_as_text()
+	var data = parse_json(content)
+	if data:
+		if data.file.selected_file && file.file_exists(data.file.selected_file):
+			expose_csv(data.file.data)
+			file_path_label.text = data.file.selected_file
+		if data.raw_text:
+			raw_text_input.text = data.raw_text
+	file.close()
