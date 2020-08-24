b44dd78a740e8e7ad9d365051cd8178ef0ada00d
Francois Boivin <boivin.92@gmail.com>
2020-08-24 09:45:02 -0400

    renaming to remove "SQL" from code

--- UI/MainScreen.gd
+++ UI/MainScreen.gd
@@ -6,7 +6,7 @@ const user_prefs = "user://user_prefs.json"
 var prefs = {}
 
 onready var csvMethod = $MarginContainer/HBoxContainer/LeftColumn/VBoxContainer/CsvMethod
-onready var templateTabs = $MarginContainer/HBoxContainer/LeftColumn/SqlTemplatesTabs
+onready var templateTabs = $MarginContainer/HBoxContainer/LeftColumn/TemplatesTabs
 onready var generatedTabs = $MarginContainer/HBoxContainer/OutputWindow
 onready var separatorLineEdit = $MarginContainer/HBoxContainer/LeftColumn/VBoxContainer/OptionsContainer/VBoxContainer2/CsvSeparator/LineEdit
 onready var outputLinesSpinBox = $MarginContainer/HBoxContainer/LeftColumn/VBoxContainer/OptionsContainer/VBoxContainer2/OutputLines/SpinBox
